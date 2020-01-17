fa3929d5bede621e819e6f0f16d26ae4fc04f5b9
asmit22 <asmitadhikari22@gmail.com>
2020-01-17 14:05:58 +0545

    Notification with body and icon

--- app/src/main/java/com/asmit/notification/MainActivity.java
+++ app/src/main/java/com/asmit/notification/MainActivity.java
@@ -1,12 +1,20 @@
 package com.asmit.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.asmit.notification.createchannel.CreateChannel;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManagerCompat notificationManagerCompat;
 
     private Button ch1;
     private Button ch2;
@@ -15,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        notificationManagerCompat = NotificationManagerCompat.from(this);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
 
         ch1 = findViewById(R.id.btnC1);
         ch2 = findViewById(R.id.btnC2);
@@ -22,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         ch1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                DisplayNotification();
 
             }
         });
@@ -30,8 +42,34 @@ public class MainActivity extends AppCompatActivity {
         ch2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                DisplayNotification2();
+
+
             }
         });
+    }
+
+    private void DisplayNotification2() {
+
+        Notification notification = new NotificationCompat.Builder(this, CreateChannel.CHANNEL_2)
+                .setSmallIcon(R.drawable.ic_warning_black_24dp)
+                .setContentTitle("Second Message")
+                .setContentText("Second message body")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(2, notification);
+    }
+
+    private void DisplayNotification() {
+
+        Notification notification = new NotificationCompat.Builder(this, CreateChannel.CHANNEL_1)
+                .setSmallIcon(R.drawable.ic_notifications_active_black_24dp)
+                .setContentTitle("First Message")
+                .setContentText("First message body")
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(1, notification);
     }
 }
